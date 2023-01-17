781b1068d8ca662bbb4dc32cf34f376ed370802c
Daniil Kuznetsov <daniilbacardi@yandex.ru>
2023-01-17 21:05:28 +0300

    fix: исправления и доработки по review 2

--- src/tasksTypes/Epic.java
+++ src/tasksTypes/Epic.java
@@ -11,10 +11,6 @@ public class Epic extends Task {
         super(name, description);
     }
 
-    public Epic(int id, String name, String description, String status) {
-        super(id, name, description, status);
-    }
-
     public void addSubtaskId(int id) {
         subtaskIds.add(id);
     }
