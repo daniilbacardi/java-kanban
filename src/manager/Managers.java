771b8ff3e6289910b02001947056872f68490964
Daniil Kuznetsov <daniilbacardi@yandex.ru>
2023-01-29 10:35:40 +0300

    fix: добавил переменную для числа макс просмотров

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -6,6 +6,7 @@ public class Managers {
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
+
     public static HistoryManager getDefaultHistory() {
         return historyManager;
     }
